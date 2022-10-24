58fdcfe24c1e0a9f4c05a6557c6077c61b7895d8
MikeB1996 <mikelbarko2@gmail.com>
2022-10-24 21:33:50 +0200

    added working resize function

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const Grid=document.querySelector("#gridCont");
 const ResetBut=document.querySelector("#resetB");
+const ResizeBut=document.querySelector("#resizeB");
 let numRows=80;
 let numCul=80;
+//let slider = document.getElementById("myRange");
 ResetBut.addEventListener('click',function (){
     resetGrid();
 });
+ResizeBut.addEventListener('click',function (){
+    resizeGrid();
+});
+
+
+
 
 function resetGrid(){
     let allGridDivs = document.querySelectorAll(".gridDiv").forEach(div => {
@@ -12,7 +20,35 @@ function resetGrid(){
     })
 }
 
+
 function resizeGrid(){
+    let slider = document.getElementById("myRange");
+    dim = slider.value;
+    alert(slider.value);
+    cancelGrid();
+    reCreateGrid(dim);
+   
+}
+
+function cancelGrid(){
+    let allGridDivs = document.querySelectorAll(".gridDiv").forEach(div => {
+        div.remove();
+    });
+}
+
+function reCreateGrid(dimension){
+    let i;
+    let j;
+    for (i=0;i<dimension;i++){
+        const addDiv=createDiv(i,dimension);
+        Grid.appendChild(addDiv);
+        for (j=0;j<dimension;j++){
+            const addDiv1=createDivVer(j,dimension);
+            addDiv.appendChild(addDiv1);
+            addDiv1.addEventListener('mouseenter', ()=> addDiv1.style.backgroundColor="red");
+        }
+    }
+    
 
 }
    
@@ -20,11 +56,12 @@ function resizeGrid(){
 function createGrid(){
     let i;
     let j;
-    for (i=0;i<numRows;i++){
-        const addDiv=createDiv(i);
+    let dim=32;
+    for (i=0;i<dim;i++){
+        const addDiv=createDiv(i,dim);
         Grid.appendChild(addDiv);
-        for (j=0;j<numCul;j++){
-            const addDiv1=createDivVer(j);
+        for (j=0;j<dim;j++){
+            const addDiv1=createDivVer(j,dim);
             addDiv.appendChild(addDiv1);
             addDiv1.addEventListener('mouseenter', ()=> addDiv1.style.backgroundColor="red");
         }
@@ -33,15 +70,18 @@ function createGrid(){
 
 }
 
+
+
+
 function mouseOverDiv(){
 
 }
 
-function createDivVer(num){
+function createDivVer(num,dim){
     const addDiv= document.createElement("div");
     addDiv.setAttribute("id",`div${num}`);
-    addDiv.style.width=`${600/numRows}px`;
-    addDiv.style.height=`${600/numCul}px`;
+    addDiv.style.width=`${600/dim}px`;
+    addDiv.style.height=`${600/dim}px`;
     addDiv.style.backgroundColor="blue";
     addDiv.style.borderColor="red";
     //addDiv.style.border="solid";
@@ -51,11 +91,11 @@ function createDivVer(num){
     return addDiv;
 }
 
-function createDiv(num){
+function createDiv(num,dim){
     const addDiv= document.createElement("div");
     addDiv.setAttribute("id",`divs${num}`);
-    addDiv.style.width=`${600/numRows}px`;
-    addDiv.style.height=`${600/numCul}px`;
+    addDiv.style.width=`${600/dim}px`;
+    addDiv.style.height=`${600/dim}px`;
     addDiv.style.float="none";
     addDiv.style.display="inline-table";
     //addDiv.style.border="solid"
